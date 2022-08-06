5d3d07508e7053cd7122713060d6e778cfd869b1
Dharan Costa <dharancosta@gmail.com>
2022-08-06 13:46:52 -0300

    First Commit: Creating Java Spring Boot project and adding dependecies

---
+++ src/main/java/com/cardPokemon/CardGamePokemonApplication.java
@@ -0,0 +1,13 @@
+package com.cardPokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGamePokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGamePokemonApplication.class, args);
+	}
+
+}
